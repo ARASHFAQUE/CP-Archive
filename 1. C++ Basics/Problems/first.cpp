ee05226adaf9d08b0bfac0ed6e32fe847be76ee0
Md. Ashfaqur Rahman <55973326+ARASHFAQUE@users.noreply.github.com>
2022-07-02 18:12:20 +0600

    Infinite Commit

---
+++ 1. C++ Basics/Problems/first.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1 <= l <= 10^9
+// 1 <= b <= 10^9
+
+int main(){
+
+    //long long int l, b; cin >> l >> b;
+    //cout << l * b << endl;
+    int l, b; cin >> l >> b;
+    cout << l * 1LL * b << endl;
+}
